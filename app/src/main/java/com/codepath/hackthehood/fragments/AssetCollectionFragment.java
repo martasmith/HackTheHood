09282ca5f01b57f1876c8f7647668493812233fb
Marta Smith <smith.marta@gmail.com>
2014-10-16 23:31:20 -0700

    Added "take photo/ photo upload" functionality to AssetCollectionFragment

--- app/src/main/java/com/codepath/hackthehood/fragments/AssetCollectionFragment.java
+++ app/src/main/java/com/codepath/hackthehood/fragments/AssetCollectionFragment.java
@@ -1,6 +1,10 @@
 package com.codepath.hackthehood.fragments;
 
+import java.io.File;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.util.Log;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,13 +12,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Spinner;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.os.Environment;
+import android.provider.MediaStore;
 import com.codepath.hackthehood.R;
 import com.codepath.hackthehood.activities.AssetCollectionActivity;
 import com.codepath.hackthehood.activities.WebpageCollectionActivity;
@@ -33,7 +43,15 @@ public class AssetCollectionFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private final int REQUEST_CODE = 20;
+    private final int REQUEST_CODE_WEB_CONTENT = 10;
+    private final int REQUEST_CODE_TAKE_PHOTO_HEADER = 20;
+    private final int REQUEST_CODE_TAKE_PHOTO_LOGO = 21;
+    private final int REQUEST_CODE_TAKE_PHOTO_MORE = 22;
+    private final int REQUEST_CODE_UPLOAD_PHOTO_HEADER = 30;
+    private final int REQUEST_CODE_UPLOAD_PHOTO_LOGO = 31;
+    private final int REQUEST_CODE_UPLOAD_PHOTO_MORE = 32;
+    public final String APP_TAG = "HTH_app";
+
 
     private String mParam1;
     private String mParam2;
@@ -42,6 +60,8 @@ public class AssetCollectionFragment extends Fragment {
     private Spinner sprBusinessType;
     private ImageView ivHeader, ivLogo, ivMore, checkPage1,checkPage2,checkPage3;
     private Button btnPage1,btnPage2,btnPage3, btnSubmit;
+    private PopupMenu popup;
+    public String photoFileName = "photo.jpg";
 
 
     //private OnFragmentInteractionListener mListener;
@@ -101,6 +121,10 @@ public class AssetCollectionFragment extends Fragment {
         setupPageCreationListener(btnPage1,"checkPage1");
         setupPageCreationListener(btnPage2,"checkPage2");
         setupPageCreationListener(btnPage3,"checkPage3");
+
+        setupImgUploadListener(ivHeader,REQUEST_CODE_TAKE_PHOTO_HEADER,REQUEST_CODE_UPLOAD_PHOTO_HEADER);
+        setupImgUploadListener(ivLogo,REQUEST_CODE_TAKE_PHOTO_LOGO,REQUEST_CODE_UPLOAD_PHOTO_LOGO);
+        setupImgUploadListener(ivMore,REQUEST_CODE_TAKE_PHOTO_MORE,REQUEST_CODE_UPLOAD_PHOTO_MORE);
         return v;
     }
 
@@ -113,7 +137,7 @@ public class AssetCollectionFragment extends Fragment {
                 title = btn.getText().toString();
                 i.putExtra("title",title);
                 i.putExtra("tickImgName",checkPage);
-                startActivityForResult(i, REQUEST_CODE);
+                startActivityForResult(i, REQUEST_CODE_WEB_CONTENT);
 
             }
         });
@@ -128,11 +152,94 @@ public class AssetCollectionFragment extends Fragment {
         });
     }
 
+    private void setupImgUploadListener(final ImageView img, final int cameraRequestCode, final int galleryRequestCode) {
+        img.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //Creating the instance of PopupMenu
+                popup = new PopupMenu(getActivity(), img);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater().inflate(R.menu.image_popup_menu, popup.getMenu());
+
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        //Toast.makeText(getActivity(),"You Clicked : " + item.getTitle(),Toast.LENGTH_SHORT).show();
+                     switch (item.getItemId()) {
+                         case R.id.takePhoto:
+                         {
+                             capturePhoto(cameraRequestCode);
+                         }
+                         break;
+                         case R.id.useExisting:
+                         {
+                             pickFromGallery(galleryRequestCode);
+                         }
+                         break;
+                     }
+
+                     return true;
+                    }
+                });
+
+                popup.show();//showing popup menu
+            }
+        });//closing the setOnClickListener method
+
+    }
+
+    private void capturePhoto(int requestCode) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(photoFileName)); // set the image file name
+        // Start the image capture intent to take photo
+        startActivityForResult(intent, requestCode);
+    }
+
+    private void getCapturedPhoto(int resultCode, ImageView img) {
+        if (resultCode == getActivity().RESULT_OK) {
+            //extract photo that was just taken by the camera
+            Uri takenPhotoUri = getPhotoFileUri(photoFileName);
+            // by this point we have the camera photo on disk
+            Bitmap takenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+            // Load the taken image into a preview
+            img.setImageBitmap(takenImage);
+        } else { // Result was a failure
+            Toast.makeText(getActivity(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void pickFromGallery(int requestCode) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Bring up gallery to select a photo
+        startActivityForResult(intent, requestCode);
+    }
+
+    private void getPickedFromGallery(Intent data, ImageView img) {
+        //extract photo that was just picked from the gallery
+        if (data != null) {
+            Uri photoUri = data.getData();
+            // Do something with the photo based on Uri
+            Bitmap selectedImage = null;
+            try {
+                selectedImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), photoUri);
+                // Load the selected image into a preview
+                img.setImageBitmap(selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(getActivity(), "No photo was selected", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         //super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE && resultCode == getActivity().RESULT_OK) {
+        if (requestCode == REQUEST_CODE_WEB_CONTENT && resultCode == getActivity().RESULT_OK) {
             tickImgName = data.getExtras().getString("tickImgName");
             //Toast.makeText(getActivity(), "tickImgName= " + tickImgName, Toast.LENGTH_LONG).show();
             if (tickImgName.equals("checkPage1")) {
@@ -142,11 +249,42 @@ public class AssetCollectionFragment extends Fragment {
             } else if (tickImgName.equals("checkPage3")) {
                 checkPage3.setVisibility(View.VISIBLE);
             }
-
         }
+        else if (requestCode == REQUEST_CODE_TAKE_PHOTO_HEADER) {
+                getCapturedPhoto(resultCode,ivHeader);
+        }
+        else if (requestCode == REQUEST_CODE_TAKE_PHOTO_LOGO) {
+                getCapturedPhoto(resultCode,ivLogo);
+        }
+        else if (requestCode == REQUEST_CODE_TAKE_PHOTO_MORE) {
+                getCapturedPhoto(resultCode,ivMore);
+        }
+        else if (requestCode == REQUEST_CODE_UPLOAD_PHOTO_HEADER && resultCode == getActivity().RESULT_OK) {
+            getPickedFromGallery(data,ivHeader);
+        }
+        else if (requestCode == REQUEST_CODE_UPLOAD_PHOTO_LOGO && resultCode == getActivity().RESULT_OK) {
+            getPickedFromGallery(data,ivLogo);
+        }
+        else if (requestCode == REQUEST_CODE_UPLOAD_PHOTO_MORE && resultCode == getActivity().RESULT_OK) {
+            getPickedFromGallery(data,ivMore);
+        }
+
     }
 
+    // Returns the Uri for a photo stored on disk given the fileName
+    public Uri getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        File mediaStorageDir = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), APP_TAG);
 
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
+    }
     /*
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
