adcb5528d29cc9b3056b9e6ce37babfcd9008878
Marta Smith <smith.marta@gmail.com>
2014-10-18 19:32:35 -0700

    Added Parse integration to BusinessFormFragment and made adjustment to the UI

--- app/src/main/java/com/codepath/hackthehood/fragments/BusinessFormFragment.java
+++ app/src/main/java/com/codepath/hackthehood/fragments/BusinessFormFragment.java
@@ -1,8 +1,7 @@
 package com.codepath.hackthehood.fragments;
 
-import android.app.Activity;
+
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,58 +9,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.codepath.hackthehood.util.MultiSelectionSpinner;
 import com.codepath.hackthehood.R;
-import com.codepath.hackthehood.activities.AssetCollectionActivity;
-import com.codepath.hackthehood.activities.BusinessFormActivity;
 import com.codepath.hackthehood.activities.ConfirmationActivity;
+import com.codepath.hackthehood.models.Address;
+import com.codepath.hackthehood.models.User;
+import com.codepath.hackthehood.models.Website;
+import com.codepath.hackthehood.util.MultiSelectionSpinner;
+import com.parse.ParseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BusinessFormFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BusinessFormFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
 public class BusinessFormFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private TextView tvBformTime, tvBusinessInfo, tvContactInfo;
     private EditText etBusinessName, etBusinessStreet, etBusinessCity, etBusinessZip,
                      etBusinessPhone,etContactName,etContactPhone,etContactEmail;
     private MultiSelectionSpinner sprOnlinePresence;
     private Button btnSubmitBusinessForm;
+    private String businessName, businessStreet,businessCity,businessZip,businessPhone,onlinePresence,contactName,contactPhone,contactEmail;
 
-    //private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BusinessFormFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BusinessFormFragment newInstance(String param1, String param2) {
-        BusinessFormFragment fragment = new BusinessFormFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
     public BusinessFormFragment() {
         // Required empty public constructor
     }
@@ -69,29 +36,23 @@ public class BusinessFormFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_business_form, container, false);
         View v = inflater.inflate(R.layout.fragment_business_form, container, false);
         etBusinessName = (EditText) v.findViewById(R.id.etBusinessName);
         etBusinessStreet = (EditText) v.findViewById(R.id.etBusinessStreet);
         etBusinessCity = (EditText) v.findViewById(R.id.etBusinessCity);
         etBusinessZip = (EditText) v.findViewById(R.id.etBusinessZip);
+        etBusinessPhone = (EditText) v.findViewById(R.id.etBusinessPhone);
         sprOnlinePresence = (MultiSelectionSpinner)  v.findViewById(R.id.sprOnlinePresence);
         etContactName = (EditText) v.findViewById(R.id.etContactName);
         etContactPhone = (EditText) v.findViewById(R.id.etContactPhone);
         etContactEmail = (EditText) v.findViewById(R.id.etContactEmail);
         btnSubmitBusinessForm = (Button)  v.findViewById(R.id.btnSubmit);
-        String [] spinnerContentArr = { "Website", "Online Store", "Facebook Page","Twitter Account","Google Listing", "Yelp","Other"};
+        String [] spinnerContentArr = {"Website", "Online Store", "Facebook Page","Twitter Account","Google Listing", "Yelp","Other"};
         sprOnlinePresence.setItems(spinnerContentArr);
         setupSubmitListener();
         return v;
@@ -101,61 +62,71 @@ public class BusinessFormFragment extends Fragment {
         btnSubmitBusinessForm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //String s = sprOnlinePresence.getSelectedItemsAsString();
-                //Toast.makeText(getActivity(), s , Toast.LENGTH_LONG).show();
-                Intent i = new Intent(getActivity(), ConfirmationActivity.class);
-                startActivity(i);
+                if (submitBusinessForm()) {
+                    Intent i = new Intent(getActivity(), ConfirmationActivity.class);
+                    startActivity(i);
+                }
             }
         });
     }
 
+    private boolean submitBusinessForm() {
+        businessName = etBusinessName.getText().toString();
+        businessStreet = etBusinessStreet.getText().toString();
+        businessCity = etBusinessCity.getText().toString();
+        businessZip = etBusinessZip.getText().toString();
+        businessPhone = etBusinessPhone.getText().toString();
+        onlinePresence = sprOnlinePresence.getSelectedItemsAsString();
+        contactName = etContactName.getText().toString();
+        contactPhone = etContactPhone.getText().toString();
+        contactEmail = etContactEmail.getText().toString();
 
-    /*
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+        if (businessName.isEmpty()) {
+            Toast.makeText(getActivity(), "Business name is required", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (businessPhone.isEmpty()) {
+            Toast.makeText(getActivity(), "Business phone is required", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (contactName.isEmpty()) {
+            Toast.makeText(getActivity(), "Contact name is required", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (contactEmail.isEmpty()) {
+            Toast.makeText(getActivity(), "Contact email is required", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (contactPhone.isEmpty()) {
+            Toast.makeText(getActivity(), "Contact phone is required", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+
+            //get current user
+            User user = (User) ParseUser.getCurrentUser();
+
+            //set  Parse user values
+            user.setFullName(contactName);
+            user.setEmail(contactEmail);
+            user.setPhoneNumber(contactPhone);
+            //user.setApplicationStatus(User.ApplicationStatus.PENDING_REVIEW);
+            user.saveEventually();
+
+            //set business address
+            Address businessAddress = new Address();
+            businessAddress.setStreetAddress(businessStreet);
+            businessAddress.setCity(businessCity);
+            businessAddress.setPostalCode(businessZip);
+            businessAddress.saveEventually();
+
+            //set website values
+            Website website = user.getWebsite();
+            website.setBusinessName(businessName);
+            website.setAddress(businessAddress);
+            website.setPhoneNumber(businessPhone);
+            website.setOnlinePresenceType(onlinePresence);
+            website.saveEventually();
+
+            return true;
         }
+
+
     }
-
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-    */
-
-
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    /*
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
-    */
-
-
 
 }
