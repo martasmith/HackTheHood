2d61f90e742d3325f38f4168029dcddb383225a9
Ravi Rani <ravi@eventbrite.com>
2014-10-23 00:10:45 -0700

    Updated app logo
    Used their logo on Twitter, which fits better overall.

--- app/src/main/java/com/codepath/hackthehood/fragments/TestFragment.java
+++ app/src/main/java/com/codepath/hackthehood/fragments/TestFragment.java
@@ -1,13 +1,14 @@
 package com.codepath.hackthehood.fragments;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import com.codepath.hackthehood.R;
 
 /**
