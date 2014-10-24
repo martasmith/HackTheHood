1888fe7be93985959ab3f55f3ce9565b8e24a10e
Thomas Harte <thomas.harte@gmail.com>
2014-10-23 21:46:06 -0700

    Ensured all Parse stuff is asynchronous and that exceptions are caught. Simplified while I was here.

--- app/src/main/java/com/codepath/hackthehood/fragments/NetworkFragment.java
+++ app/src/main/java/com/codepath/hackthehood/fragments/NetworkFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.support.v4.app.Fragment;
 
 import com.codepath.hackthehood.activities.NetworkFragmentContainer;
+import com.parse.SaveCallback;
 
 /**
  * Created by thomasharte on 21/10/2014.
@@ -42,6 +43,23 @@ public abstract class NetworkFragment extends Fragment {
     }
 
     public void refresh() {
+        fetch(false);
+    }
 
+    private boolean isLoading = false;
+    public void fetch(boolean onlyIfNeeded) {
+        if(isLoading) return;
+        isLoading = true;
+        doFetch(onlyIfNeeded);
+    }
+
+    protected void doFetch(boolean onlyIfNeeded) {
+    }
+
+    protected void setFetchIsFinished() {
+        isLoading = false;
+    }
+
+    public void submit(SaveCallback saveCallback) {
     }
 }
--- app/src/main/java/com/codepath/hackthehood/fragments/WebsitePageCollectionFragment.java
+++ app/src/main/java/com/codepath/hackthehood/fragments/WebsitePageCollectionFragment.java
@@ -19,45 +19,49 @@ import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import com.codepath.hackthehood.R;
-import com.codepath.hackthehood.models.ImageResource;
+import com.codepath.hackthehood.models.ParseHelper;
 import com.codepath.hackthehood.models.User;
 import com.codepath.hackthehood.models.WebsitePage;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 
 public class WebsitePageCollectionFragment extends NetworkFragment {
 
-    private final static int REQUEST_CODE_TAKE_PHOTO_1 = 20;
-    private final static int REQUEST_CODE_TAKE_PHOTO_2 = 21;
-    private final static int REQUEST_CODE_TAKE_PHOTO_3 = 22;
-    private final static int REQUEST_CODE_UPLOAD_PHOTO_1 = 30;
-    private final static int REQUEST_CODE_UPLOAD_PHOTO_2 = 31;
-    private final static int REQUEST_CODE_UPLOAD_PHOTO_3 = 32;
-    public final String APP_TAG = "HTH_app";
+    private final static int REQUEST_CODE_TAKE_PHOTO    = 20;
+    private final static int REQUEST_CODE_SELECT_PHOTO  = 30;
+    public final static String APP_TAG = "HTH_app";
 
-    // TODO: Rename and change types of parameters
-    private String tickImgName, title, pageText,designerNotes;
     private EditText etPageText, etDesignerNotes;
-    private ImageView ivFileUpload1,ivFileUpload2,ivFileUpload3;
+    private List<ImageView> imageViews;
     private Button btnAddSite;
     private PopupMenu popup;
-    private Bitmap photo1Bitmap, photo2Bitmap, photo3Bitmap;
     private WebsitePage page;
 
-
+    private final static String TICK_IMAGE_NAME = "tickImgName";
+    private final static String TITLE = "title";
+    private final static String PAGE_INDEX = "pageIndex";
     public static WebsitePageCollectionFragment newInstance(String tickImgName, String title, int pageIndex) {
         WebsitePageCollectionFragment fragment = new WebsitePageCollectionFragment();
+
         Bundle args = new Bundle();
-        args.putString("tickImgName", tickImgName);
-        args.putString("title", title);
-        args.putInt("pageIndex", pageIndex);
+        args.putString  (TICK_IMAGE_NAME, tickImgName);
+        args.putString  (TITLE, title);
+        args.putInt     (PAGE_INDEX, pageIndex);
         fragment.setArguments(args);
+
         return fragment;
     }
+
     public WebsitePageCollectionFragment() {
         // Required empty public constructor
     }
@@ -65,86 +69,136 @@ public class WebsitePageCollectionFragment extends NetworkFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            tickImgName = getArguments().getString("tickImgName");
-            title = getArguments().getString("title");
+        fetch(true);
+    }
 
-            int pageIndex = getArguments().getInt("pageIndex");
-            User user = (User) ParseUser.getCurrentUser();
-            try {
-                user.fetch();
-                user.getWebsite().fetch();
-            } catch(Exception e) {}
-            page = user.getWebsite().getWebsitePages().get(pageIndex);
-            try {
-                page.fetch();
-            } catch(Exception e) {}
+    @Override
+    public void doFetch(boolean onlyIfNeeded) {
+        super.doFetch(onlyIfNeeded);
 
-        }
+        final int pageIndex = getArguments().getInt(PAGE_INDEX);
+        final User user = (User) ParseUser.getCurrentUser();
+        incrementNetworkActivityCount();
+        ParseHelper.fetchObjectsInBackgroundInSerial(true,
+                new Iterator<ParseObject>() {
+                    private int index = 0;
+
+                    @Override
+                    public boolean hasNext() {
+                        return index != 3;
+                    }
+
+                    @Override
+                    public ParseObject next() {
+                        index++;
+                        switch (index) {
+                            default:
+                                return user;
+                            case 2:
+                                return user.getWebsite();
+                            case 3:
+                                return user.getWebsite().getWebsitePages().get(pageIndex);
+                        }
+                    }
+
+                    @Override
+                    public void remove() {
+                    }
+                }, new GetCallback() {
+                    @Override
+                    public void done(ParseObject parseObject, ParseException e) {
+                        setFetchIsFinished();
+                        decrementNetworkActivityCount();
+
+                        if (e != null) {
+                            didReceiveNetworkException(e);
+                            return;
+                        }
+
+                        page = user.getWebsite().getWebsitePages().get(pageIndex);
+                        populateView();
+                    }
+                });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View v = inflater.inflate(R.layout.fragment_webpage_collection,container,false);
+
         etPageText = (EditText) v.findViewById(R.id.etPageText);
         etDesignerNotes = (EditText) v.findViewById(R.id.etDesignerNotes);
-        ivFileUpload1 = (ImageView) v.findViewById(R.id.imgFile1);
-        ivFileUpload2 = (ImageView) v.findViewById(R.id.imgFile2);
-        ivFileUpload3 = (ImageView) v.findViewById(R.id.imgFile3);
         btnAddSite = (Button) v.findViewById(R.id.btnAddSite);
+
+        imageViews = new ArrayList<ImageView>();
+        imageViews.add((ImageView)v.findViewById(R.id.imgFile1));
+        imageViews.add((ImageView)v.findViewById(R.id.imgFile2));
+        imageViews.add((ImageView)v.findViewById(R.id.imgFile3));
+
         setupAddSiteListener();
-        setupImgUploadListener(ivFileUpload1, "photoFile1.jpg", REQUEST_CODE_TAKE_PHOTO_1, REQUEST_CODE_UPLOAD_PHOTO_1);
-        setupImgUploadListener(ivFileUpload2,"photoFile2.jpg",REQUEST_CODE_TAKE_PHOTO_2,REQUEST_CODE_UPLOAD_PHOTO_2);
-        setupImgUploadListener(ivFileUpload3,"photoFile3.jpg",REQUEST_CODE_TAKE_PHOTO_3,REQUEST_CODE_UPLOAD_PHOTO_3);
+        for(int c = 0; c < 3; c++)
+            setupImgUploadListener(c);
+
+        fetch(true);
         return v;
+    }
+
+    private void populateView() {
+
     }
 
     private void setupAddSiteListener() {
         btnAddSite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                submitWebpageData();
-                Intent data = new Intent();
-                data.putExtra("tickImgName", tickImgName);
-                getActivity().setResult(getActivity().RESULT_OK, data);
-                getActivity().finish();
+                submit(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
 
+                        if(e != null) {
+                            Intent data = new Intent();
+                            data.putExtra("tickImgName", getArguments().getString(TICK_IMAGE_NAME));
+                            getActivity().setResult(getActivity().RESULT_OK, data);
+                            getActivity().finish();
+                        }
+
+                    }
+                });
             }
         });
     }
 
-    private void submitWebpageData() {
+    @Override
+    public void submit(final SaveCallback saveCallback) {
 
-        pageText = etPageText.getText().toString();
-        designerNotes = etDesignerNotes.getText().toString();
+        page.setNotes(etDesignerNotes.getText().toString());
+        page.setTitle(getArguments().getString(TITLE));
+        page.setText(etPageText.getText().toString());
 
-        if (page != null) {
-            page.setNotes(designerNotes);
-            page.setTitle(title);
-            page.setText(pageText);
-            page.setNotes(designerNotes);
-            List<ImageResource> imageResources = page.getImageResources();
-            int num = imageResources.size();
-            imageResources.get(0).setBitmap(photo1Bitmap,null);
-            imageResources.get(1).setBitmap(photo2Bitmap,null);
-            imageResources.get(2).setBitmap(photo2Bitmap,null);
-
-            try {
-                page.save();
-            } catch(Exception e) {}
-        }
+        incrementNetworkActivityCount();
+        page.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                decrementNetworkActivityCount();
+                if (e != null) {
+                    didReceiveNetworkException(e);
+                }
+                saveCallback.done(e);
+            }
+        });
     }
 
-    private void setupImgUploadListener(final ImageView img, final String photoFileName, final int cameraRequestCode, final int galleryRequestCode) {
-        img.setOnClickListener(new View.OnClickListener() {
+    private void setupImgUploadListener(final int index) {
+        final ImageView imageView = imageViews.get(index);
+        imageView.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                //Creating the instance of PopupMenu
-                popup = new PopupMenu(getActivity(), img);
-                //Inflating the Popup using xml file
+                //Create the instance of PopupMenu
+                popup = new PopupMenu(getActivity(), imageView);
+
+                //Inflate the Popup using xml file
                 popup.getMenuInflater().inflate(R.menu.image_popup_menu, popup.getMenu());
 
                 //registering popup with OnMenuItemClickListener
@@ -153,119 +207,83 @@ public class WebsitePageCollectionFragment extends NetworkFragment {
                         //Toast.makeText(getActivity(),"You Clicked : " + item.getTitle(),Toast.LENGTH_SHORT).show();
                         switch (item.getItemId()) {
                             case R.id.takePhoto:
-                            {
-                                capturePhoto(photoFileName,cameraRequestCode);
-                            }
-                            break;
+                                takePhoto(index);
+                                break;
                             case R.id.useExisting:
-                            {
-                                pickFromGallery(photoFileName,galleryRequestCode);
-                            }
-                            break;
+                                pickFromGallery(index);
+                                break;
                         }
 
                         return true;
                     }
                 });
 
-                popup.show();//showing popup menu
+                popup.show(); //show the popup
             }
         });//closing the setOnClickListener method
-
     }
 
-    private void capturePhoto(String photoFileName, int requestCode) {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode >= REQUEST_CODE_TAKE_PHOTO && requestCode <= REQUEST_CODE_TAKE_PHOTO + 3) {
+            onTakePhotoResult(requestCode - REQUEST_CODE_TAKE_PHOTO, resultCode, data);
+        }
+
+        if(requestCode >= REQUEST_CODE_SELECT_PHOTO && requestCode <= REQUEST_CODE_SELECT_PHOTO + 3) {
+            onSelectPhotoResult(requestCode - REQUEST_CODE_SELECT_PHOTO, resultCode, data);
+        }
+    }
+    private void takePhoto(int index) {
         // create Intent to take a picture and return control to the calling application
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(photoFileName)); // set the image file name
-        // Start the image capture intent to take photo
-        startActivityForResult(intent, requestCode);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(index)); // set the image file name
+        startActivityForResult(intent, REQUEST_CODE_TAKE_PHOTO + index);
     }
 
-    private void getCapturedPhoto(String photoFileName, int resultCode, ImageView img) {
+    private void pickFromGallery(int index) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Bring up gallery to select a photo
+        startActivityForResult(intent, REQUEST_CODE_SELECT_PHOTO + index);
+    }
+
+    private void onTakePhotoResult(int index, int resultCode, Intent data) {
         if (resultCode == getActivity().RESULT_OK) {
             //extract photo that was just taken by the camera
-            Uri takenPhotoUri = getPhotoFileUri(photoFileName);
-            // by this point we have the camera photo on disk
-            if (photoFileName.equals("photoFile1.jpg")) {
-                photo1Bitmap = BitmapFactory.decodeFile(takenPhotoUri.getPath());
-                // Load the taken image into a preview
-                img.setImageBitmap(photo1Bitmap);
-            } else if (photoFileName.equals("photoFile2.jpg")) {
-                photo2Bitmap = BitmapFactory.decodeFile(takenPhotoUri.getPath());
-                // Load the taken image into a preview
-                img.setImageBitmap(photo2Bitmap);
-            } else if (photoFileName.equals("photoFile3.jpg")) {
-                photo3Bitmap = BitmapFactory.decodeFile(takenPhotoUri.getPath());
-                // Load the taken image into a preview
-                img.setImageBitmap(photo3Bitmap);
-            }
+            setBitmap(index, BitmapFactory.decodeFile(getPhotoFileUri(index).getPath()));
         } else { // Result was a failure
             Toast.makeText(getActivity(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void pickFromGallery(String photoFileName, int requestCode) {
-        // Create intent for picking a photo from the gallery
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        // Bring up gallery to select a photo
-        startActivityForResult(intent, requestCode);
-    }
-
-    private void getPickedFromGallery(Intent data, ImageView img) {
+    private void onSelectPhotoResult(int index, int resultCode, Intent data) {
         //extract photo that was just picked from the gallery
-        if (data != null) {
-            Uri photoUri = data.getData();
-            try {
-                if (img.getDrawable() == ivFileUpload1.getDrawable()) {
-                    photo1Bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), photoUri);
-                    // Load the selected image into a preview
-                    img.setImageBitmap(photo1Bitmap);
-                } else if (img.getDrawable() == ivFileUpload2.getDrawable()) {
-                    photo2Bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), photoUri);
-                    // Load the selected image into a preview
-                    img.setImageBitmap(photo2Bitmap);
-                } else if (img.getDrawable() == ivFileUpload3.getDrawable()) {
-                    photo3Bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), photoUri);
-                    // Load the selected image into a preview
-                    img.setImageBitmap(photo3Bitmap);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
+        try {
+            setBitmap(index, MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData()));
+        } catch (IOException e) {
             Toast.makeText(getActivity(), "No photo was selected", Toast.LENGTH_SHORT).show();
         }
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        //super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_TAKE_PHOTO_1) {
-            getCapturedPhoto("photoFile1.jpg",resultCode,ivFileUpload1);
-        }
-        else if (requestCode == REQUEST_CODE_TAKE_PHOTO_2) {
-            getCapturedPhoto("photoFile2.jpg",resultCode,ivFileUpload2);
-        }
-        else if (requestCode == REQUEST_CODE_TAKE_PHOTO_3) {
-            getCapturedPhoto("photoFile3.jpg",resultCode,ivFileUpload3);
-        }
-        else if (requestCode == REQUEST_CODE_UPLOAD_PHOTO_1 && resultCode == getActivity().RESULT_OK) {
-            getPickedFromGallery(data,ivFileUpload1);
-        }
-        else if (requestCode == REQUEST_CODE_UPLOAD_PHOTO_2 && resultCode == getActivity().RESULT_OK) {
-            getPickedFromGallery(data,ivFileUpload2);
-        }
-        else if (requestCode == REQUEST_CODE_UPLOAD_PHOTO_3 && resultCode == getActivity().RESULT_OK) {
-            getPickedFromGallery(data,ivFileUpload3);
-        }
-
+    private void setBitmap(int index, Bitmap bitmap) {
+        imageViews.get(index).setImageBitmap(bitmap);
+        incrementNetworkActivityCount();
+        page.getImageResources().get(0).setBitmap(bitmap, new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                decrementNetworkActivityCount();
+                if(e != null) {
+                    didReceiveNetworkException(e);
+                }
+            }
+        });
     }
+
     // Returns the Uri for a photo stored on disk given the fileName
-    public Uri getPhotoFileUri(String fileName) {
+    public Uri getPhotoFileUri(int index) {
         // Get safe storage directory for photos
         File mediaStorageDir = new File(
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), APP_TAG);
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), APP_TAG);
 
         // Create the storage directory if it does not exist
         if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
@@ -273,6 +291,6 @@ public class WebsitePageCollectionFragment extends NetworkFragment {
         }
 
         // Return the file target for the photo based on filename
-        return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
+        return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + "image" + index + ".jpg"));
     }
 }
