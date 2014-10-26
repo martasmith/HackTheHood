08582c62046e7c389611e622fddc3c7f0e2e6c34
Thomas Harte <thomas.harte@gmail.com>
2014-10-25 20:22:19 -0700

    Added a progress bar and display of Parse exceptions.

--- app/src/main/java/com/codepath/hackthehood/activities/MainNavigationActivity.java
+++ app/src/main/java/com/codepath/hackthehood/activities/MainNavigationActivity.java
@@ -3,21 +3,25 @@ package com.codepath.hackthehood.activities;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.codepath.hackthehood.R;
 import com.codepath.hackthehood.fragments.ApplicationFragment;
 import com.codepath.hackthehood.fragments.FragmentNavigationDrawer;
 
-public class MainNavigationActivity extends ActionBarActivity {
+public class MainNavigationActivity extends ActionBarActivity implements NetworkFragmentContainer {
     private FragmentNavigationDrawer navDrawer;
+    private ProgressBar pbLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_navigation);
+
+        pbLoading = (ProgressBar)findViewById(R.id.pbLoading);
 
         // Find our drawer view
         navDrawer = (FragmentNavigationDrawer) findViewById(R.id.drawer_layout);
@@ -44,5 +48,28 @@ public class MainNavigationActivity extends ActionBarActivity {
         super.onConfigurationChanged(newConfig);
         // Pass any configuration change to the drawer toggles
         navDrawer.getDrawerToggle().onConfigurationChanged(newConfig);
+    }
+
+    private int activityCount = 0;
+    @Override
+    public void incrementActivityCount() {
+        if(activityCount == 0) {
+            pbLoading.setVisibility(View.VISIBLE);
+        }
+        activityCount++;
+    }
+
+    @Override
+    public void decrementActivityCount() {
+        activityCount--;
+        if(activityCount == 0) {
+            pbLoading.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void didReceiveException(Exception e) {
+        // This is imaginative, I know
+        Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
     }
 }
