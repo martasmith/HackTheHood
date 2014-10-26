20b7244fcb1b725dcc68248a29cf57dc401abe0d
Ravi Rani <ravi@eventbrite.com>
2014-10-25 23:55:59 -0700

    Added drawer item to show email and allow logout

--- app/src/main/java/com/codepath/hackthehood/adapters/NavDrawerListAdapter.java
+++ app/src/main/java/com/codepath/hackthehood/adapters/NavDrawerListAdapter.java
@@ -1,14 +1,19 @@
 package com.codepath.hackthehood.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.codepath.hackthehood.R;
+import com.codepath.hackthehood.activities.PitchDeckActivity;
 import com.codepath.hackthehood.models.NavDrawerItem;
+import com.parse.Parse;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
 
@@ -17,17 +22,25 @@ import java.util.ArrayList;
  */
 public class NavDrawerListAdapter extends BaseAdapter {
 
-    private Context context;
     private ArrayList<NavDrawerItem> navDrawerItems;
+    private LayoutInflater mInflater;
+    private Context mContext;
+
 
     public NavDrawerListAdapter(Context context, ArrayList<NavDrawerItem> navDrawerItems){
-        this.context = context;
+        mContext = context;
         this.navDrawerItems = navDrawerItems;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
     }
 
     @Override
     public int getCount() {
-        return navDrawerItems.size();
+        return navDrawerItems.size() + 1 /* one for spacer and one for user email address*/;
     }
 
     @Override
@@ -43,13 +56,29 @@ public class NavDrawerListAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
-            LayoutInflater mInflater = LayoutInflater.from(context);
-            convertView = mInflater.inflate(R.layout.drawer_nav_item, null);
+            switch (position) {
+                case 0:
+                    convertView = mInflater.inflate(R.layout.drawer_user_item, null);
+                    TextView tvEmail = (TextView) convertView.findViewById(R.id.tvEmail);
+                    tvEmail.setText(ParseUser.getCurrentUser().getEmail());
+                    ImageView ivLogout = (ImageView) convertView.findViewById(R.id.ivLogout);
+                    ivLogout.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ParseUser.logOut();
+                            mContext.startActivity(new Intent(mContext, PitchDeckActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                        }
+                    });
+                    break;
+
+                default:
+                    convertView = mInflater.inflate(R.layout.drawer_nav_item, null);
+                    TextView txtTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+                    txtTitle.setText(navDrawerItems.get(position - 1).getTitle());
+                    break;
+
+            }
         }
-
-        TextView txtTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        txtTitle.setText(navDrawerItems.get(position).getTitle());
-
         return convertView;
     }
 
